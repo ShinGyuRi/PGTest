cff74b5912a016ccfeac2e70ff4a0b39db72dbcb
shingyuri <s.conast@gmail.com>
2016-07-05 17:08:00 +0900

    Add ListCouponResult.java and dialog_coupon_use.xml

---
+++ movingkey/TiltPhoto/TiltCode_New/app/src/main/java/tiltcode/movingkey/com/tiltcode_new/Model/ListCouponResult.java
@@ -0,0 +1,14 @@
+package tiltcode.movingkey.com.tiltcode_new.Model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Gyul on 2016-07-04.
+ */
+public class ListCouponResult {
+
+    @SerializedName("coupon")
+    public List<Coupon> coupon;
+}
